65129095779a999123b5c96b066faec14a985259
Shnk22 <128026677+Shnk22@users.noreply.github.com>
2024-06-12 17:56:40 +0530

    Update product_controller.py

--- controller/product_controller.py
+++ controller/product_controller.py
@@ -2,3 +2,6 @@ from app import app
 @app.route('/product/add')
 def product():
     return "Add your products"
+
+"Product controller:This is just for an experiment purpose"
+"You can use it for your own projects"
